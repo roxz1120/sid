841a97c000c1bd111e315496b19d71812b331b5b
roxz1120 <anahulmotwani@outlook.com>
2024-12-30 02:56:06 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8017046831:AAGu9Ga1xr-E9T-YqLCcmpgoFOZW_QjKCmg")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Roxz_gaming")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7352008650")
+
+
+
+
+
+
+
+  
